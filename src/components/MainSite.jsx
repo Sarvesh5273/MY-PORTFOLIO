9cccfe13efe28c31d20b2fd58587483250de57ef
Sarvesh <techzstarofficial@gmail.com>
2025-10-03 00:24:19 +0530

    prelodder making.

---
+++ src/components/MainSite.jsx
@@ -0,0 +1,8 @@
+export default function MainSite() {
+  return (
+    <div style={{ color: "white", textAlign: "center", marginTop: "50px" }}>
+      <h1>Welcome to my website!</h1>
+      <p>This is your main content area.</p>
+    </div>
+  );
+}
