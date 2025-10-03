0a21045b7198b0a3de89ab45b80b73f719819682
Sarvesh <techzstarofficial@gmail.com>
2025-10-03 15:08:13 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import Preloader from "./components/Preloader.jsx";
 import MainSite from "./components/MainSite.jsx";
 import Navbar from "./components/Navbar.jsx";
-import InteractiveStarfield from "./components/InteractiveStarfield.jsx"; // Import the starfield
 
 export default function App() {
   const [loading, setLoading] = useState(true);
@@ -13,7 +12,6 @@ export default function App() {
 
   return (
     <>
-      <InteractiveStarfield />
       <Navbar />
       <MainSite />
     </>
